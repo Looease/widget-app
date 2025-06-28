c86d0bbb97978cd256376960d39b3f773ae3fd92
Louise <lgilligang@gmail.com>
2025-06-28 19:34:50 +0100

    add routes

---
+++ server/routes/routes.spec.tsx
@@ -0,0 +1,16 @@
+import { describe, test } from "vitest";
+
+describe('routes', () =>{
+    test('should allow the user to add widgets', () => {
+
+    });
+    test('should allow the user to delete widgets', () => {
+
+    });
+    test('should return empty array if widgets not found', () => {
+
+    });
+    test('should handle errors', () => {
+
+    });
+})
