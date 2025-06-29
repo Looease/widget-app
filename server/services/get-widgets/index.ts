251369bd209dcd6b9150640bcbd7068080d33b7c
Louise <lgilligang@gmail.com>
2025-06-29 17:45:14 +0100

    refactor

--- server/services/get-widgets/index.ts
+++ server/services/get-widgets/index.ts
@@ -1,11 +1,1 @@
 export * from './getWidgets'
-
-//docker run --name widget-app-database -e POSTGRES_PASSWORD=trumpet -p 5432:5432 -d postgres
-
-// docker exec -it widget-app-database psql -U postgres
-
-// CREATE TABLE widgets(id SERIAL PRIMARY KEY,  content VARCHAR NOT NULL);
-
-// docker start widget-app-database
-
-//\dt
