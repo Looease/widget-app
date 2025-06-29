38debb3ed3630c5d15b41a999cb3239e2700a9a5
Louise <lgilligang@gmail.com>
2025-06-29 18:07:12 +0100

    run prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import WidgetPage from './components/WidgetPage'
-import './App.css'
+import WidgetPage from "./components/WidgetPage";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <main>
-      <WidgetPage/>
-    </main>
+      <main>
+        <WidgetPage />
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,10 +1,10 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
   },
-})
+});
