edc7144b77ab98af7f3dec91aad3a17880b60c76
Louise <lgilligang@gmail.com>
2025-06-30 19:22:00 +0100

    update cors allowed origins

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   urlencoded({ extended: true }),
   cors({
-    origin: ["http://localhost:5173", `${process.env.DATABASE_URL}`],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   }),
